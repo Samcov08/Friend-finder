6e26ea70b4ae19f83ed51f03156eabfe93ac33a1
Samantha Covington <samanthanc_08@yahoo.com>
2019-10-24 22:33:00 -0500

    added images- need to fix errors

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,26 +1,32 @@
 var friends = [{
-        name: "Chase",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Chandler ",
+        photo: "https://obfriends.files.wordpress.com/2013/10/chandler-bing-friends-19337888-510-383.jpg",
         scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
     {
-        name: "Emily ",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Monica ",
+        photo: "https://obfriends.files.wordpress.com/2013/10/monica_geller.jpg",
         scores: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
     },
     {
-        name: "Emma",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Rachel Green",
+        photo: "https://e3.365dm.com/19/09/768x432/skynews-rachel-green-friends_4780471.jpg?20190920100815",
         scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
     },
     {
-        name: "Jeremy",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Ross Geller",
+        photo: "https://i2.wp.com/metro.co.uk/wp-content/uploads/2019/09/PRI_85461411.jpg?quality=90&strip=all&zoom=1&resize=644%2C779&ssl=1",
         scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
     },
     {
-        name: "Brittany",
-        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name: "Phoebe Buffay",
+        photo: "https://live.staticflickr.com/2501/3985925066_74d5d7d323_b.jpg",
         scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    },
+
+    {
+        name: "Joey Tribbiani",
+        photo: "https://img.maximummedia.ie/joe_ie/eyJkYXRhIjoie1widXJsXCI6XCJodHRwOlxcXC9cXFwvbWVkaWEtam9lLm1heGltdW1tZWRpYS5pZS5zMy5hbWF6b25hd3MuY29tXFxcL3dwLWNvbnRlbnRcXFwvdXBsb2Fkc1xcXC8yMDE1XFxcLzEwXFxcLzI5MTExMzEyXFxcL0pvZXkuanBnXCIsXCJ3aWR0aFwiOjY0MCxcImhlaWdodFwiOjM2MCxcImRlZmF1bHRcIjpcImh0dHBzOlxcXC9cXFwvd3d3LmpvZS5pZVxcXC9hc3NldHNcXFwvaW1hZ2VzXFxcL2pvZVxcXC9uby1pbWFnZS5wbmc_aWQ9MjY0YTJkYmUzNzBmMmM2NzVmY2RcIixcIm9wdGlvbnNcIjpbXX0iLCJoYXNoIjoiMGFiNTQ3MmQ3YzBhOWQ1ZDZlMmE1YWRhODM5ODRlZTc4N2Q4YmQ4MSJ9/joey.jpg",
+        scores: [5, 2, 3, 4, 1, 4, 4, 4, 5, 2]
     }
 ];

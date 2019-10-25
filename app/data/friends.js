987ae066370f30936eda1a91e628c7c7641c02f7
Samantha Covington <samanthanc_08@yahoo.com>
2019-10-24 21:05:09 -0500

    added apiRoute.js

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var friends = [{
+        name: "Chase",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        name: "Emily ",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        name: "Emma",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+    },
+    {
+        name: "Jeremy",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+    },
+    {
+        name: "Brittany",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    }
+];
